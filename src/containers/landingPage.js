917034942e24a39dd7bedba6759a8101d8ca8677
byarustev <byarus45@gmail.com>
2019-07-11 19:54:42 +0300

    initial files

---
+++ src/containers/landingPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+
+                <div className={'app-header'}>
+                    {/*<h1>lets farm</h1>*/}
+                </div>
+
+        );
+    }
+}
+
+export default LandingPage;
